11a5096539527a12b5e5c9ae013e26b649a7f71a
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2025-02-03 13:36:33 +0100

    Update prog python interface graphique emc.py

--- prog python interface graphique emc.py
+++ prog python interface graphique emc.py
@@ -27,23 +27,23 @@ def bouton_action_1():
 
     # Titre de la nouvelle page
     titre_jeu = ttk.Label(fenetre_jeu, text="Ajoutez vos pseudos", font=("Arial", 18, "bold"), background="#1c1c1c", foreground="white")
-    titre_jeu.pack(pady=20, fill=tk.X)
+    titre_jeu.pack(pady=20)
 
     # Champ de texte pour le pseudo
     pseudo_entry = ttk.Entry(fenetre_jeu, font=("Arial", 16))
-    pseudo_entry.pack(pady=20, fill=tk.X, padx=20)
+    pseudo_entry.pack(pady=20)
 
     # Bouton pour ajouter le pseudo
     btn_ajouter_pseudo = ttk.Button(fenetre_jeu, text="Ajouter Pseudo", command=ajouter_pseudo, style="Accent.TButton")
-    btn_ajouter_pseudo.pack(pady=10, fill=tk.X, padx=20)
+    btn_ajouter_pseudo.pack(pady=10)
 
     # Listbox pour afficher les pseudos ajoutés
     listbox_pseudos = tk.Listbox(fenetre_jeu, font=("Arial", 14), height=5)
-    listbox_pseudos.pack(pady=20, fill=tk.BOTH, expand=True, padx=20)
+    listbox_pseudos.pack(pady=20)
 
     # Bouton pour quitter la page (retour à la page principale)
     btn_quitter_jeu = ttk.Button(fenetre_jeu, text="Retour à l'accueil", command=fenetre_jeu.destroy, style="Large.Accent.TButton")
-    btn_quitter_jeu.pack(pady=20, fill=tk.X, padx=20)
+    btn_quitter_jeu.pack(pady=20)
 
 def bouton_action_2():
     messagebox.showinfo("Quitter", "Action pour le bouton 2")
@@ -72,18 +72,18 @@ style.map(
 
 # Ajout d'un titre
 titre = ttk.Label(fenetre, text="La démocratie han", font=("Arial", 18, "bold"), anchor="center", background="#1c1c1c", foreground="white")
-titre.pack(pady=20, fill=tk.X)
+titre.pack(pady=20)
 
 # Création d'un cadre pour les boutons
 cadre_boutons = ttk.Frame(fenetre)
-cadre_boutons.pack(expand=True, fill=tk.BOTH, padx=20, pady=20)
+cadre_boutons.place(relx=0.5, rely=0.5, anchor="center")
 
 # Ajout des boutons "Jouer" et "Quitter"
 btn_1 = ttk.Button(cadre_boutons, text="Jouer", command=bouton_action_1, style="Large.Accent.TButton")
-btn_1.pack(pady=15, fill=tk.X)
+btn_1.pack(pady=15)
 
 btn_2 = ttk.Button(cadre_boutons, text="Quitter", command=fenetre.destroy, style="Large.Accent.TButton")
-btn_2.pack(pady=15, fill=tk.X)
+btn_2.pack(pady=15)
 
 # Mentions légales en bas à gauche
 mention_license = ttk.Label(fenetre, text="Projet d'EMC sous licence ", font=("Arial", 8), background="#1c1c1c", foreground="white")
