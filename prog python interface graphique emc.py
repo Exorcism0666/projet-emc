68509b4709f8cda704392f60e5c0687c2079f86b
Exorcism0666 <46537034+Exorcism0666@users.noreply.github.com>
2025-02-24 17:24:50 +0100

    Update prog python interface graphique emc.py

--- prog python interface graphique emc.py
+++ prog python interface graphique emc.py
@@ -137,4 +137,15 @@ btn_jouer = ttk.Button(cadre_boutons, style="Large.Accent.TButton", text="Jouer"
 btn_quitter = ttk.Button(cadre_boutons, style="Large.Accent.TButton", text="Quitter", command=fenetre.destroy)
 fenetre.after(1500, lambda: (btn_jouer.pack(pady=15), btn_quitter.pack(pady=15)))
 
+# Mentions légales
+mention_license = ttk.Label(fenetre, text="Projet d'EMC sous licence ", font=("Arial", 8), background="#1c1c1c", foreground="white")
+mention_license.place(relx=0.0, rely=1.0, anchor="sw", x=10, y=-30)
+
+lien_mit = tk.Label(fenetre, text="MIT", fg="white", bg="#1c1c1c", font=("Arial", 8, "underline"), cursor="hand2")
+lien_mit.place(relx=0.0, rely=1.0, anchor="sw", x=134, y=-28)
+lien_mit.bind("<Button-1>", lambda e: ouvrir_lien_mit())
+
+mention_copyright = ttk.Label(fenetre, text="Copyright © Doisne Lilou, Dubus Yanis, Ryan Cordier, Alban Bloise", font=("Arial", 8), background="#1c1c1c", foreground="white")
+mention_copyright.place(relx=0.0, rely=1.0, anchor="sw", x=10, y=-10)
+
 fenetre.mainloop()
