fa80a2173c6cab9384799bb8924f292c171ece3f
Nayriou <150892068+Nayriou@users.noreply.github.com>
2025-05-06 12:14:13 +0200

    changement du nom du jeu

--- constants.py
+++ constants.py
@@ -3,5 +3,5 @@ BACKGROUND_COLOR = "#1c1c1c"  # Couleur de fond
 FOREGROUND_COLOR = "white"    # Couleur du texte
 
 # Textes
-TITRE = "🎭 DémocraTroll\nÀ vous de jouer (ou de manipuler !)"
-DESCRIPTION = "Un jeu où la démocratie règne… enfin, jusqu'à ce que quelqu’un triche !"
+TITRE = "🎭 DémocraQuiz\nÀ vous de jouer!"
+DESCRIPTION = "Un jeu où la démocratie règne !"
